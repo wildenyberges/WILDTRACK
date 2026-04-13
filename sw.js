1d78e7c526eb1999a62b70169236fb135048decc
wildenyberges <wildenyberges@gmail.com>
2026-04-12 23:34:36 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'wildtrack-v1';
+const ASSETS = [
+  '/WILDTRACK/',
+  '/WILDTRACK/index.html',
+  '/WILDTRACK/manifest.json',
+  '/WILDTRACK/icon-192.png',
+  '/WILDTRACK/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
