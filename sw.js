e65e524fe3d068c0a49cc29a0103a514947a8e17
wildenyberges <wildenyberges@gmail.com>
2026-04-13 08:09:29 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,17 @@
-const CACHE = 'wildtrack-v1';
-const ASSETS = [
-  '/WILDTRACK/',
-  '/WILDTRACK/index.html',
-  '/WILDTRACK/manifest.json',
-  '/WILDTRACK/icon-192.png',
-  '/WILDTRACK/icon-512.png'
-];
+const CACHE = 'wildtrack-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
